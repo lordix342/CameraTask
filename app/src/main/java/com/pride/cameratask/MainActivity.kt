981b04d6f264f1dbeb39a20019a910d143a4a82b
lordix342 <105501284+lordix342@users.noreply.github.com>
2022-07-11 20:40:52 +0300

    update permissions

--- app/src/main/java/com/pride/cameratask/MainActivity.kt
+++ app/src/main/java/com/pride/cameratask/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pride.cameratask
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -8,39 +9,53 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
-    private lateinit var plauncher: ActivityResultLauncher<String>
-    private var checked : MutableState<Boolean> = mutableStateOf(false)
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) shouldShowCamera.value = true
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val cameraExecutor = Executors.newSingleThreadExecutor()
+
         setContent {
-            if (checked.value) {
+            if (shouldShowCamera.value) {
                 CameraView(
                     executor = cameraExecutor,
                     onError = { Log.e("error", "View error: $it") }
                 )
             }
         }
-        checkPermission()
-    }
-    private fun checkPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA) != 0) {
-            permissionListener()
-            plauncher.launch(Manifest.permission.CAMERA)
-        } else checked.value = true
+        requestCameraPermission()
     }
 
-    private fun permissionListener() {
-        plauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            if (!it) checkPermission()
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                shouldShowCamera.value = true
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
     }
 }
+
